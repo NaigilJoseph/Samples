80ccc8d15322b623f1afd5978922a90972cd86f5
Naigil Joseph <naigil@gmail.com>
2019-04-03 16:02:27 +0530

    Added Core project for performance testing.

---
+++ PInvokePerformance/pInvokePerformanceCore/MainViewModelCore.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Apex.MVVM;
+using System.ComponentModel;
+
+namespace pInvokePerformanceCore
+{
+    public class MainViewModelCore : ViewModel
+    {
+        public MainViewModelCore()
+        {
+            //  Set up the background worker.
+            backgroundWorker.DoWork += new DoWorkEventHandler(backgroundWorker_DoWork);
+            backgroundWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorker_RunWorkerCompleted);
+
+            //  Create the view model command.
+            runTestsCommand = new ViewModelCommand(DoRunTests, true);
+        }
+
+        void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //  Set the view model properties.
+            PerformanceTestCore test = e.Result as PerformanceTestCore;
+            Unmanaged_Test1_Result = test.Unmanaged_Test1_Time;
+            Unmanaged_Test2_Result = test.Unmanaged_Test2_Time;
+            Unmanaged_Test3_Result = test.Unmanaged_Test3_Time;
+            ManagedInteface_Test1_Result = test.ManagedInterface_Test1_Time;
+            ManagedInteface_Test2_Result = test.ManagedInterface_Test2_Time;
+            ManagedInteface_Test3_Result = test.ManagedInterface_Test3_Time;
+            PInvoke_Test1_Result = test.PInvoke_Test1_Time;
+            PInvoke_Test2_Result = test.PInvoke_Test2_Time;
+            PInvoke_Test3_Result = test.PInvoke_Test3_Time;
+
+            //  We can now run the command again.
+            runTestsCommand.CanExecute = true;
+        }
+
+        void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //  Create a performance test, set the iterations.
+            PerformanceTestCore test = new PerformanceTestCore();
+            test.TestCount = Iterations;
+
+            //  Run the performance test.
+            test.RunTests();
+
+            //  Set the result of the worker.
+            e.Result = test;
+        }
+
+        private void DoRunTests()
+        {
+            //  The test cannot run again till we finish.
+            runTestsCommand.CanExecute = false;
+
+            //  Run the background worker.
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        private BackgroundWorker backgroundWorker = new BackgroundWorker();
+
+
+        private NotifyingProperty IterationsProperty =
+          new NotifyingProperty("Iterations", typeof(ulong), 1000000UL);
+
+        public ulong Iterations
+        {
+            get { return (ulong)GetValue(IterationsProperty); }
+            set { SetValue(IterationsProperty, value); }
+        }
+
+
+        private NotifyingProperty Unmanaged_Test1_ResultProperty =
+          new NotifyingProperty("Unmanaged_Test1_Result", typeof(double), default(double));
+
+        public double Unmanaged_Test1_Result
+        {
+            get { return (double)GetValue(Unmanaged_Test1_ResultProperty); }
+            set { SetValue(Unmanaged_Test1_ResultProperty, value); }
+        }
+
+        private NotifyingProperty Unmanaged_Test2_ResultProperty =
+          new NotifyingProperty("Unmanaged_Test2_Result", typeof(double), default(double));
+
+        public double Unmanaged_Test2_Result
+        {
+            get { return (double)GetValue(Unmanaged_Test2_ResultProperty); }
+            set { SetValue(Unmanaged_Test2_ResultProperty, value); }
+        }
+
+        private NotifyingProperty Unmanaged_Test3_ResultProperty =
+          new NotifyingProperty("Unmanaged_Test3_Result", typeof(double), default(double));
+
+        public double Unmanaged_Test3_Result
+        {
+            get { return (double)GetValue(Unmanaged_Test3_ResultProperty); }
+            set { SetValue(Unmanaged_Test3_ResultProperty, value); }
+        }
+
+        private NotifyingProperty ManagedInteface_Test1_ResultProperty =
+          new NotifyingProperty("ManagedInteface_Test1_Result", typeof(double), default(double));
+
+        public double ManagedInteface_Test1_Result
+        {
+            get { return (double)GetValue(ManagedInteface_Test1_ResultProperty); }
+            set { SetValue(ManagedInteface_Test1_ResultProperty, value); }
+        }
+
+        private NotifyingProperty ManagedInteface_Test2_ResultProperty =
+          new NotifyingProperty("ManagedInteface_Test2_Result", typeof(double), default(double));
+
+        public double ManagedInteface_Test2_Result
+        {
+            get { return (double)GetValue(ManagedInteface_Test2_ResultProperty); }
+            set { SetValue(ManagedInteface_Test2_ResultProperty, value); }
+        }
+
+        private NotifyingProperty ManagedInteface_Test3_ResultProperty =
+          new NotifyingProperty("ManagedInteface_Test3_Result", typeof(double), default(double));
+
+        public double ManagedInteface_Test3_Result
+        {
+            get { return (double)GetValue(ManagedInteface_Test3_ResultProperty); }
+            set { SetValue(ManagedInteface_Test3_ResultProperty, value); }
+        }
+
+        private NotifyingProperty PInvoke_Test1_ResultProperty =
+          new NotifyingProperty("PInvoke_Test1_Result", typeof(double), default(double));
+
+        public double PInvoke_Test1_Result
+        {
+            get { return (double)GetValue(PInvoke_Test1_ResultProperty); }
+            set { SetValue(PInvoke_Test1_ResultProperty, value); }
+        }
+
+        private NotifyingProperty PInvoke_Test2_ResultProperty =
+          new NotifyingProperty("PInvoke_Test2_Result", typeof(double), default(double));
+
+        public double PInvoke_Test2_Result
+        {
+            get { return (double)GetValue(PInvoke_Test2_ResultProperty); }
+            set { SetValue(PInvoke_Test2_ResultProperty, value); }
+        }
+
+        private NotifyingProperty PInvoke_Test3_ResultProperty =
+          new NotifyingProperty("PInvoke_Test3_Result", typeof(double), default(double));
+
+        public double PInvoke_Test3_Result
+        {
+            get { return (double)GetValue(PInvoke_Test3_ResultProperty); }
+            set { SetValue(PInvoke_Test3_ResultProperty, value); }
+        }
+
+
+
+        private ViewModelCommand runTestsCommand;
+
+        public ViewModelCommand RunTestsCommand
+        {
+            get { return runTestsCommand; }
+        }
+    }
+}
---
+++ PInvokePerformance/pInvokePerformanceCore/PerformanceTestCore.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace pInvokePerformanceCore
+{
+    public class PerformanceTestCore
+    {
+        [DllImport("TraditionalAPI.dll")]
+        private static extern void TA_IncrementCounter();
+
+        [DllImport("TraditionalAPI.dll")]
+        private static extern double TA_CalculateSquareRoot(double value);
+
+        [DllImport("TraditionalAPI.dll")]
+        private static extern double TA_DotProduct(double[] threeTuple1, double[] threeTuple2);
+
+        [DllImport("TraditionalAPI.dll")]
+        private static extern uint TA_Test1(double count);
+
+        [DllImport("TraditionalAPI.dll")]
+        private static extern double TA_Test2(double count);
+
+        [DllImport("TraditionalAPI.dll")]
+        private static extern double TA_Test3(double count);
+
+        public void RunTests()
+        {
+            double dResult = 0;
+            uint uResult = 0;
+            //  Run the unmanged tests.
+            stopwatch.Restart();
+            uResult = TA_Test1(testCount);
+            stopwatch.Stop();
+            Unmanaged_Test1_Time = Math.Round(stopwatch.Elapsed.TotalMilliseconds, 4);
+
+            stopwatch.Restart();
+            dResult = TA_Test2(testCount);
+            stopwatch.Stop();
+            Unmanaged_Test2_Time = Math.Round(stopwatch.Elapsed.TotalMilliseconds, 4);
+
+            stopwatch.Restart();
+            dResult = TA_Test3(testCount);
+            stopwatch.Stop();
+            Unmanaged_Test3_Time = Math.Round(stopwatch.Elapsed.TotalMilliseconds, 4);
+
+            double[] threeTuple1 = new double[] { 0, 0, 0 };
+            double[] threeTuple2 = new double[] { 0, 0, 0 };
+
+            //  Run the tests through the pinvoke.
+            stopwatch.Restart();
+            for (ulong i = 1; i <= testCount; i++)
+                TA_IncrementCounter();
+            stopwatch.Stop();
+            PInvoke_Test1_Time = Math.Round(stopwatch.Elapsed.TotalMilliseconds, 4);
+
+            dResult = 0;
+            stopwatch.Restart();
+            for (ulong i = 1; i <= testCount; i++)
+            {
+                dResult += TA_CalculateSquareRoot((double)i);
+            }
+            stopwatch.Stop();
+            PInvoke_Test2_Time = Math.Round(stopwatch.Elapsed.TotalMilliseconds, 4);
+
+            dResult = 0;
+            stopwatch.Restart();
+            for (ulong i = 1; i <= testCount; i++)
+            {
+                threeTuple1[0] = i;
+                threeTuple1[1] = i;
+                threeTuple1[2] = i;
+
+                threeTuple2[0] = i;
+                threeTuple2[1] = i;
+                threeTuple2[2] = i;
+
+                dResult += TA_DotProduct(threeTuple1, threeTuple2);
+            }
+            stopwatch.Stop();
+            PInvoke_Test3_Time = Math.Round(stopwatch.Elapsed.TotalMilliseconds, 4);
+        }
+
+        private ulong testCount = 1000000;
+
+        public ulong TestCount
+        {
+            get { return testCount; }
+            set { testCount = value; }
+        }
+
+        private Stopwatch stopwatch = new Stopwatch();
+
+        public double Unmanaged_Test1_Time { get; set; }
+        public double Unmanaged_Test2_Time { get; set; }
+        public double Unmanaged_Test3_Time { get; set; }
+
+        public double ManagedInterface_Test1_Time { get; set; }
+        public double ManagedInterface_Test2_Time { get; set; }
+        public double ManagedInterface_Test3_Time { get; set; }
+
+        public double PInvoke_Test1_Time { get; set; }
+        public double PInvoke_Test2_Time { get; set; }
+        public double PInvoke_Test3_Time { get; set; }
+    }
+}
